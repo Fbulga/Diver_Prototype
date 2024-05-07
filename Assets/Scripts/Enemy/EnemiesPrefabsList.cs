3ff54320db55e9299bd64c795d68733daf600cd7
Fbulga <146501113+Fbulga@users.noreply.github.com>
2024-05-07 11:35:35 -0300

    Limpieza

--- Assets/Scripts/Enemy/EnemiesPrefabsList.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "EnemyTypes", menuName = "Enemy/EnemyTypes")]
-public class EnemiesPrefabsList : ScriptableObject
-{
-    [field: SerializeField] public List<Enemy> enemiesPrefabs { get; private set; } = new();
-}
